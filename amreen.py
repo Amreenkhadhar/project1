75f4525c355b329d0fc9020ffcf23c381f8e4eef
Amreenkhadhar <143685546+Amreenkhadhar@users.noreply.github.com>
2023-08-31 11:01:48 +0530

    Add files via upload

---
+++ amreen.py
@@ -0,0 +1,21 @@
+Name="Amreen"
+Age=20
+Dept="Computer Science"
+SSLC_Tam=90
+SSLC_Eng=91
+SSLC_Mat=90
+SSLC_Sci=90
+SSLC_Soc=96
+SSLC_Total=SSLC_Tam+SSLC_Eng+SSLC_Mat+SSLC_Sci+SSLC_Soc
+SSLC_Avg=SSLC_Total/5
+HSC_Tam=90
+HSC_Eng=91
+HSC_Eco=90
+HSC_Comm=90
+HSC_Acc=96
+HSC_CA=99
+HSC_Total=HSC_Tam+HSC_Eng+HSC_Eco+HSC_Comm+HSC_Acc+HSC_CA
+HSC_Avg=HSC_Total/6
+print("I'm ",Name,"and I'm ",Age,"years old.""from the dept of ",Dept,"SSLC Mark avg",SSLC_Avg,"HSC Marks avg",HSC_Avg) 
+
+
